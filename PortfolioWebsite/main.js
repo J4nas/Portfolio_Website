ab3ba53d905785e094fcf5723398a2869e724177
Quoboo <jonas.beckerv1@icloud.com>
2023-11-29 17:52:58 +0100

    Push

--- PortfolioWebsite/main.js
+++ PortfolioWebsite/main.js
@@ -33,3 +33,58 @@ document.querySelector('.anidash').addEventListener('click', function() {
     window.location.href = 'https://github.com/Anidashcom';
 });
 
+document.querySelector('.lumania').addEventListener('click', function() {
+    window.location.href = 'https://github.com/Lumania-net';
+});
+
+document.querySelector('.html5').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/HTML5';
+});
+
+document.querySelector('.css3').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/Cascading_Style_Sheets';
+});
+
+document.querySelector('.js').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/JavaScript';
+});
+
+document.querySelector('.java').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/Java_(Programmiersprache)';
+});
+
+document.querySelector('.bootstrap').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/Bootstrap_(Framework)';
+});
+
+document.querySelector('.react').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/React';
+});
+
+document.querySelector('.nodejs').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/Node.js';
+});
+
+document.querySelector('.nginx').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/Nginx';
+});
+
+document.querySelector('.linux').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/Linux';
+});
+
+document.querySelector('.cloudflare').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/Cloudflare';
+});
+
+document.querySelector('.db').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/Datenbanksprache';
+});
+
+document.querySelector('.docker').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/Docker_(Software)';
+});
+
+document.querySelector('.blender').addEventListener('click', function() {
+    window.location.href = 'https://de.wikipedia.org/wiki/Blender_(Software)';
+});
